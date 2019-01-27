8926aad54088c212d3caa3e5991fcb4e69c3e8d9
Adam Radford (aradford) <aradford@cisco.com>
2019-01-28 00:28:55 +1100

    CLEUR19

---
+++ PnP-BulkConfig-128/00_pnp_devices.py
@@ -0,0 +1,71 @@
+#!/usr/bin/env python
+from __future__ import print_function
+import csv
+import json
+import requests
+import os
+import os.path
+import logging
+from argparse import ArgumentParser
+from utils import login, get, post
+#from fake import fake
+logger = logging.getLogger()
+
+def get_ip(host):
+    try:
+        ip = host['deviceInfo']['httpHeaders'][0]['value']
+    except KeyError:
+        ip =""
+    return ip
+def get_neighbour(host):
+    try:
+        link = host['deviceInfo']['neighborLinks'][0]['remoteInterfaceName']
+    except KeyError:
+        link = ''
+    return link
+
+def process(data):
+    fmt="{:13s}{:17}{:12}{:8}{:16}{:26}{:26}"
+    print(fmt.format("Name","PID","State","Source","IP Address","Neighbour Interface","Workflow Name"))
+    for host in data:
+        logging.debug('Host:{}'.format(json.dumps(host)))
+        print(fmt.format(host['deviceInfo']['name'],
+                         host['deviceInfo']['pid'],
+                         host['deviceInfo']['state'],
+                         host['deviceInfo']['source'],
+                         get_ip(host),
+                         get_neighbour(host),
+                         host['deviceInfo']['workflowName']
+                         ))
+def process_single(device):
+    print(json.dumps(device, indent=2))
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description='Select options.')
+    parser.add_argument( '--device', type=str,
+            help='serial number of device')
+    parser.add_argument('-v', action='store_true',
+                        help="verbose")
+    args = parser.parse_args()
+
+    if args.v:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(
+            '%(asctime)s %(name)-12s %(levelname)-8s %(message)s')
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+        logger.setLevel(logging.DEBUG)
+
+        # set logger
+    logger.debug("Logging enabled")
+
+    dnac = login()
+
+    if args.device:
+        # get the device
+        device = response = get(dnac, "dna/intent/api/v1/onboarding/pnp-device?name={}".format(args.device))
+        process_single(device.json())
+    else:
+        # show all devices
+        response = get(dnac, "dna/intent/api/v1/onboarding/pnp-device")
+        process(response.json())
