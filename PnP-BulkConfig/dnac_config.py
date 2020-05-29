e6a78b5adc6f05fefb24ce9c0efb5bf2fa4e8929
Anne Gentle <agentle@cisco.com>
2020-05-29 10:06:32 -0500

    Port change

--- PnP-BulkConfig/dnac_config.py
+++ PnP-BulkConfig/dnac_config.py
@@ -1,5 +1,5 @@
 import os
 DNAC= os.getenv("DNAC") or "sandboxdnac2.cisco.com"
 DNAC_USER= os.getenv("DNAC_USER") or "dnacdev"
-DNAC_PORT=os.getenv("DNAC_PORT") or 8080
+DNAC_PORT=os.getenv("DNAC_PORT") or 443
 DNAC_PASSWORD= os.getenv("DNAC_PASSWORD") or "D3v93T@wK!"
